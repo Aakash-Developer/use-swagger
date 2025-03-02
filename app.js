74be8db0c6935087dc137c96220ff6f982722087
Aakash-Developer <aakash20899macwan@gmail.com>
2025-03-02 22:32:44 +0530

    updated

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const { swaggerUi, swaggerDocs } = require("./swagger");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json()); // Middleware for parsing JSON
 
 // Connect to MongoDB
